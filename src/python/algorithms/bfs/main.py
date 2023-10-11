4f189e6bee214fc6f288df090c451706ac2d31ea
Quique Muma <quiquemuma@gmail.com>
2023-10-11 18:19:56 +0200

    Primera prueba con modo 1234

---
+++ src/python/algorithms/bfs/main.py
@@ -0,0 +1,212 @@
+#! /usr/bin/env python
+
+"""
+# Notactión
+
+## Mapa
+
+En mapa original:
+
+* 0: libre
+* 1: ocupado (muro/obstáculo)
+
+Vía código incorporamos:
+
+* 2: visitado
+* 3: start
+* 4: goal
+
+## Nodo
+
+Nós
+* -2: parentId del nodo start
+* -1: parentId del nodo goal PROVISIONAL cuando aun no se ha resuelto
+
+# Específico de implementación Python
+
+* Índices empiezan en 0
+* charMap
+"""
+
+# # Initial values are hard-coded (A nivel mapa)
+
+#FILE_NAME = "/usr/local/share/master-ipr/map1/map1.csv" # Linux-style absolute path
+#FILE_NAME = "C:\\Users\\USER_NAME\\Downloads\\master-ipr\\map1\\map1.csv" # Windows-style absolute path, note the `\\` and edit `USER_NAME`
+#FILE_NAME = "../../../../map1/map1.csv" # Linux-style relative path
+FILE_NAME = "C:\\Users\\quiqu\\Desktop\\Master\\Introducción a la planificación de robots\\Practica\\master-ipr\\map1\\map1.csv" # Windows-style relative path, note the `\\`
+START_X = 2
+START_Y = 2
+END_X = 7
+END_Y = 2
+
+# # Define Node class (A nivel grafo/nodo)
+
+class Node:
+    def __init__(self, x, y, myId, parentId):
+        self.x = x
+        self.y = y
+        self.myId = myId
+        self.parentId = parentId
+    def dump(self):
+        print("---------- x "+str(self.x)+\
+                         " | y "+str(self.y)+\
+                         " | id "+str(self.myId)+\
+                         " | parentId "+str(self.parentId))
+
+# # Mapa
+
+# ## Creamos estructura de datos para mapa
+
+charMap = []
+
+# ## Creamos función para volcar estructura de datos para mapa
+
+def dumpMap():
+    for line in charMap:
+        print(line)
+
+# ## De fichero, llenar estructura de datos de fichero (`to parse`/`parsing``) para mapa
+
+with open(FILE_NAME) as f:
+    line = f.readline()
+    while line:
+        charLine = line.strip().split(',')
+        charMap.append(charLine)
+        line = f.readline()
+
+# ## A nivel mapa, integramos la info que teníamos de start & end
+
+charMap[START_X][START_Y] = '3' # 3: start
+charMap[END_X][END_Y] = '4' # 4: goal
+
+# ## Volcamos mapa por consola
+
+dumpMap()
+
+# # Grafo búsqueda
+
+# ## Creamos el primer nodo
+init = Node(START_X, START_Y, 0, -2)
+# init.dump() # comprobar que primer nodo bien
+
+# ## `nodes` contendrá los nodos del grafo
+
+nodes = []
+
+# ## Añadimos el primer nodo a `nodes`
+
+nodes.append(init)
+
+#Creo una variable nodo en la que vamos guardando el nodo visitado
+newNode = init
+
+# ## Empieza algoritmo
+
+done = False  # clásica condición de parada del bucle `while`
+goalParentId = -1  # -1: parentId del nodo goal PROVISIONAL cuando aun no se ha resuelto
+mode = 1 #Tenemos cuatro modos de desplazamiento (uno por cada dirección)
+
+while not done:
+    print("--------------------- number of nodes: "+str(len(nodes)))
+    
+    
+    if mode == 1:
+        node = newNode #actualizo el valor del nodo con el que vamos a trabajar
+        node.dump()
+
+        # up
+        tmpX = node.x - 1
+        tmpY = node.y
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("up: GOALLLL!!!")
+            goalParentId = node.myId  # aquí sustituye por real
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("up: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+        elif ( charMap[tmpX][tmpY] == '1' or  charMap[tmpX][tmpY] == '2'): # avanzo en la misma direccion hasta chocar con un muro
+            print("up: wall found")
+            mode = 2
+        
+        dumpMap()
+            
+    if mode == 2:
+        node = newNode
+        node.dump()
+        # right
+        tmpX = node.x
+        tmpY = node.y + 1
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("right: GOALLLL!!!")
+            goalParentId = node.myId # aquí sustituye por real
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("right    : mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+        elif ( charMap[tmpX][tmpY] == '1' or  charMap[tmpX][tmpY] == '2'):
+            print("right: wall found")
+            mode = 3
+
+        dumpMap()
+
+    if mode == 3:
+        node = newNode
+        node.dump()
+        # down
+        tmpX = node.x + 1
+        tmpY = node.y
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("down: GOALLLL!!!")
+            goalParentId = node.myId # aquí sustituye por real
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("down: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+        elif ( charMap[tmpX][tmpY] == '1' or  charMap[tmpX][tmpY] == '2'):
+            print("down: wall found")
+            mode = 4
+        dumpMap()
+        
+        
+    if mode == 4:
+        node = newNode
+        node.dump()
+        # left
+        tmpX = node.x
+        tmpY = node.y - 1
+        if( charMap[tmpX][tmpY] == '4' ):
+            print("left: GOALLLL!!!")
+            goalParentId = node.myId # aquí sustituye por real
+            done = True
+            break
+        elif ( charMap[tmpX][tmpY] == '0' ):
+            print("left: mark visited")
+            newNode = Node(tmpX, tmpY, len(nodes), node.myId)
+            charMap[tmpX][tmpY] = '2'
+            nodes.append(newNode)
+        elif ( charMap[tmpX][tmpY] == '1' or  charMap[tmpX][tmpY] == '2'):
+            print("left: wall found")
+            mode = 1
+        dumpMap()
+
+# ## Display solución hallada
+
+print("%%%%%%%%%%%%%%%%%%%")
+ok = False
+while not ok:
+    for node in nodes:
+        if( node.myId == goalParentId ):
+            node.dump()
+            goalParentId = node.parentId
+            if( goalParentId == -2):
+                print("%%%%%%%%%%%%%%%%%2")
+                ok = True
